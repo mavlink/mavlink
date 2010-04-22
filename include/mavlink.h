78426e7c6658fb4c4a62e5e11d99f1168ec80107
lm <pixhawk@pixhawk02.(none)>
2010-04-22 09:37:34 +0200

    Added doxygen generation files

--- include/mavlink.h
+++ include/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Friday, April 16 2010, 10:01 UTC
+ *	 Generated on Wednesday, April 21 2010, 13:23 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
