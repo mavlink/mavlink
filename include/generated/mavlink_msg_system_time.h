0c76765ac561875f56a8a6a71a6da587f879d543
lm <pixhawk@pixhawk02.(none)>
2010-05-05 09:15:22 +0200

    Fixed naming of timestamp in SYS_TIME message

--- include/generated/mavlink_msg_system_time.h
+++ include/generated/mavlink_msg_system_time.h
@@ -4,7 +4,7 @@
 
 typedef struct __mavlink_system_time_t 
 {
-	uint64_t type; ///< Timestamp of the master clock in milliseconds since UNIX epoch.
+	uint64_t time_usec; ///< Timestamp of the master clock in microseconds since UNIX epoch.
 
 } mavlink_system_time_t;
 
@@ -13,30 +13,30 @@ typedef struct __mavlink_system_time_t
 /**
  * @brief Send a system_time message
  *
- * @param type Timestamp of the master clock in milliseconds since UNIX epoch.
+ * @param time_usec Timestamp of the master clock in microseconds since UNIX epoch.
  * @return length of the message in bytes (excluding serial stream start sign)
  */
-static inline uint16_t mavlink_msg_system_time_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, uint64_t type)
+static inline uint16_t mavlink_msg_system_time_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, uint64_t time_usec)
 {
 	msg->msgid = MAVLINK_MSG_ID_SYSTEM_TIME;
 	uint16_t i = 0;
 
-	i += put_uint64_t_by_index(type, i, msg->payload); //Timestamp of the master clock in milliseconds since UNIX epoch.
+	i += put_uint64_t_by_index(time_usec, i, msg->payload); //Timestamp of the master clock in microseconds since UNIX epoch.
 
 	return mavlink_finalize_message(msg, system_id, component_id, i);
 }
 
 static inline uint16_t mavlink_msg_system_time_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_system_time_t* system_time)
 {
-	return mavlink_msg_system_time_pack(system_id, component_id, msg, system_time->type);
+	return mavlink_msg_system_time_pack(system_id, component_id, msg, system_time->time_usec);
 }
 
 #ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
 
-static inline void mavlink_msg_system_time_send(mavlink_channel_t chan, uint64_t type)
+static inline void mavlink_msg_system_time_send(mavlink_channel_t chan, uint64_t time_usec)
 {
 	mavlink_message_t msg;
-	mavlink_msg_system_time_pack(mavlink_system.sysid, mavlink_system.compid, &msg, type);
+	mavlink_msg_system_time_pack(mavlink_system.sysid, mavlink_system.compid, &msg, time_usec);
 	mavlink_send_uart(chan, &msg);
 }
 
@@ -44,11 +44,11 @@ static inline void mavlink_msg_system_time_send(mavlink_channel_t chan, uint64_t
 // MESSAGE SYSTEM_TIME UNPACKING
 
 /**
- * @brief Get field type from system_time message
+ * @brief Get field time_usec from system_time message
  *
- * @return Timestamp of the master clock in milliseconds since UNIX epoch.
+ * @return Timestamp of the master clock in microseconds since UNIX epoch.
  */
-static inline uint64_t mavlink_msg_system_time_get_type(const mavlink_message_t* msg)
+static inline uint64_t mavlink_msg_system_time_get_time_usec(const mavlink_message_t* msg)
 {
 	generic_64bit r;
 	r.b[7] = (msg->payload)[0];
@@ -64,5 +64,5 @@ static inline uint64_t mavlink_msg_system_time_get_type(const mavlink_message_t*
 
 static inline void mavlink_msg_system_time_decode(const mavlink_message_t* msg, mavlink_system_time_t* system_time)
 {
-	system_time->type = mavlink_msg_system_time_get_type(msg);
+	system_time->time_usec = mavlink_msg_system_time_get_time_usec(msg);
 }
--- include/mavlink.h
+++ include/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Tuesday, May 4 2010, 13:20 UTC
+ *	 Generated on Wednesday, May 5 2010, 06:41 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
