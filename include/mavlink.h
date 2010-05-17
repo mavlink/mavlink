777b154c3b546a3afb267effab6733b27ecd1d56
pixhawk <pixhawk@pixhawk04.(none)>
2010-05-17 21:51:46 +0200

    Extended system state message

--- include/mavlink.h
+++ include/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Friday, May 14 2010, 13:16 UTC
+ *	 Generated on Montag, Mai 17 2010, 11:43 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
