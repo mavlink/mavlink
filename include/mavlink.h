088823339b1d4022efa8ee7d8e16e7eeb33c0fbb
pixhawk <pixhawk@student.ethz.ch>
2010-06-26 16:07:35 +0200

    Fixed include headers

--- include/mavlink.h
+++ include/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Saturday, June 26 2010, 11:18 UTC
+ *	 Generated on Saturday, June 26 2010, 13:33 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
@@ -13,7 +13,7 @@ extern "C" {
 
 #include "protocol.h"
 
-#define MAVLINK_ENABLED_
+#define MAVLINK_ENABLED_PIXHAWK_MESSAGES
 
 #include "generated/mavlink_msg_heartbeat.h"
 #include "generated/mavlink_msg_boot.h"
@@ -57,6 +57,29 @@ extern "C" {
 #include "generated/mavlink_msg_position_controller_output.h"
 #include "generated/mavlink_msg_statustext.h"
 #include "generated/mavlink_msg_debug.h"
+#include "generated/mavlink_msg_set_altitude.h"
+#include "generated/mavlink_msg_request_data_stream.h"
+#include "generated/mavlink_msg_request_dynamic_gyro_calibration.h"
+#include "generated/mavlink_msg_request_static_calibration.h"
+#include "generated/mavlink_msg_manual_control.h"
+#include "generated/mavlink_msg_attitude_control.h"
+#include "generated/mavlink_msg_debug_vect.h"
+#include "generated/mavlink_msg_set_cam_shutter.h"
+#include "generated/mavlink_msg_image_triggered.h"
+#include "generated/mavlink_msg_image_trigger_control.h"
+#include "generated/mavlink_msg_image_available.h"
+#include "generated/mavlink_msg_vision_position_estimate.h"
+#include "generated/mavlink_msg_position_control_setpoint_set.h"
+#include "generated/mavlink_msg_position_control_setpoint.h"
+#include "generated/mavlink_msg_marker.h"
+#include "generated/mavlink_msg_raw_aux.h"
+#include "generated/mavlink_msg_aux_status.h"
+#include "generated/mavlink_msg_control_status.h"
+#include "generated/mavlink_msg_watchdog_heartbeat.h"
+#include "generated/mavlink_msg_watchdog_process_info.h"
+#include "generated/mavlink_msg_watchdog_process_status.h"
+#include "generated/mavlink_msg_watchdog_command.h"
+#include "generated/mavlink_msg_pattern_detected.h"
 #ifdef __cplusplus
 }
 #endif
