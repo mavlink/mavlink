902d32665eec0af2e46275b2d847a699a9de087a
lm <pixhawk@pixhawk02.(none)>
2010-07-13 21:32:27 +0200

    Cleanup commit

--- include/mavlink.h
+++ include/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Tuesday, July 13 2010, 16:03 UTC
+ *	 Generated on Tuesday, July 13 2010, 19:29 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
