b87f56669db6b2b0a23855dcfb247ff5933b7624
pixhawk <pixhawk@student.ethz.ch>
2010-07-25 11:27:06 +0200

    Added <description> tag to wrap description. Does not affect compilation to C code, but allows better documentation

--- include/mavlink.h
+++ include/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Donnerstag, Juli 22 2010, 15:40 UTC
+ *	 Generated on Sunday, July 25 2010, 09:25 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
