7c2cf0f05286cd4b931196438c73d7f03b8c68b3
pixhawk <pixhawk@student.ethz.ch>
2010-10-23 23:03:31 +0200

    Redesigned MAVLink folder structure to better suit multi-project-use

---
+++ include/pixhawk/mavlink.h
@@ -0,0 +1,11 @@
+/** @file
+ *	@brief MAVLink comm protocol.
+ *	@see http://pixhawk.ethz.ch/software/mavlink
+ *	 Generated on Saturday, October 23 2010, 20:31 UTC
+ */
+#ifndef MAVLINK_H
+#define MAVLINK_H
+
+#include "pixhawk.h"
+
+#endif
---
+++ include/ualberta/ualberta.h
@@ -0,0 +1,25 @@
+/** @file
+ *	@brief MAVLink comm protocol.
+ *	@see http://pixhawk.ethz.ch/software/mavlink
+ *	 Generated on Saturday, October 23 2010, 20:38 UTC
+ */
+#ifndef UALBERTA_H
+#define UALBERTA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include "../protocol.h"
+
+#define MAVLINK_ENABLED_UALBERTA
+
+
+#include "../common/common.h"
+#include "./mavlink_msg_nav_filter_bias.h"
+#include "./mavlink_msg_request_rc_channels.h"
+#ifdef __cplusplus
+}
+#endif
+#endif
