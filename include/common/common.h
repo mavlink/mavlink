1c27feaeea21544153c113759f38d35379f669d0
Bryan Godbolt <godbolt@ece.ualberta.ca>
2010-10-25 12:23:35 -0600

    fixed ualberta rc calibration message after merge

--- include/common/common.h
+++ include/common/common.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Sunday, October 24 2010, 08:47 UTC
+ *	 Generated on Monday, October 25 2010, 17:38 UTC
  */
 #ifndef COMMON_H
 #define COMMON_H
--- include/ualberta/ualberta.h
+++ include/ualberta/ualberta.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Sunday, October 24 2010, 08:47 UTC
+ *	 Generated on Monday, October 25 2010, 17:38 UTC
  */
 #ifndef UALBERTA_H
 #define UALBERTA_H
@@ -19,6 +19,7 @@ extern "C" {
 #include "../common/common.h"
 #include "./mavlink_msg_nav_filter_bias.h"
 #include "./mavlink_msg_request_rc_channels.h"
+#include "./mavlink_msg_radio_calibration.h"
 #ifdef __cplusplus
 }
 #endif
