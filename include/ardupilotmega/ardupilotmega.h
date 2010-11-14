519ae37c02d33b9cc843abf20d64b16ee2d4779f
James Goppert <jgoppert@w7.hsl.dynalias.com>
2010-11-14 02:48:16 -0500

    Added ardupilotmega support.

---
+++ include/ardupilotmega/ardupilotmega.h
@@ -0,0 +1,24 @@
+/** @file
+ *	@brief MAVLink comm protocol.
+ *	@see http://pixhawk.ethz.ch/software/mavlink
+ *	 Generated on Sunday, November 14 2010, 07:46 UTC
+ */
+#ifndef ARDUPILOTMEGA_H
+#define ARDUPILOTMEGA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include "../protocol.h"
+
+#define MAVLINK_ENABLED_ARDUPILOTMEGA
+
+
+#include "../common/common.h"
+#include "./mavlink_msg_aux_status.h"
+#ifdef __cplusplus
+}
+#endif
+#endif
--- include/common/common.h
+++ include/common/common.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Monday, October 25 2010, 17:38 UTC
+ *	 Generated on Sunday, November 14 2010, 07:46 UTC
  */
 #ifndef COMMON_H
 #define COMMON_H
