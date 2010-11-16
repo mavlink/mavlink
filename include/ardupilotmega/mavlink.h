7485d63c76bb38cd315eac86d00cf418114a2e04
James Goppert <jgoppert@w7.hsl.dynalias.com>
2010-11-16 18:00:39 -0500

    Merged common messages and waypoint struct update.

--- include/ardupilotmega/mavlink.h
+++ include/ardupilotmega/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Sunday, November 14 2010, 17:05 UTC
+ *	 Generated on Sunday, November 14 2010, 16:32 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
