26a845613229fbb0cb8bee589c12f477fd30a42f
James Goppert <jgoppert@l1.hsl.dynalias.com>
2011-08-05 04:14:29 -0400

    Added MAV_ROI_NONE.

--- include/common/common.h
+++ include/common/common.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://qgroundcontrol.org/mavlink/
- *	 Generated on Friday, August 5 2011, 07:37 UTC
+ *	 Generated on Friday, August 5 2011, 08:13 UTC
  */
 #ifndef COMMON_H
 #define COMMON_H
@@ -81,10 +81,11 @@ enum MAV_DATA_STREAM
 /** @brief   The ROI (region of interest) for the vehicle. This can be                 be used by the vehicle for camera/vehicle attitude alignment (see                 MAV_CMD_NAV_ROI).              */
 enum MAV_ROI
 {
-	MAV_ROI_WPNEXT=0, /* Point toward next waypoint. | */
-	MAV_ROI_WPINDEX=1, /* Point toward given waypoint. | */
-	MAV_ROI_LOCATION=2, /* Point toward fixed location. | */
-	MAV_ROI_TARGET=3, /* Point toward of given id. | */
+	MAV_ROI_NONE=0, /* No region of interest. | */
+	MAV_ROI_WPNEXT=1, /* Point toward next waypoint. | */
+	MAV_ROI_WPINDEX=2, /* Point toward given waypoint. | */
+	MAV_ROI_LOCATION=3, /* Point toward fixed location. | */
+	MAV_ROI_TARGET=4, /* Point toward of given id. | */
 	MAV_ROI_ENUM_END
 };
 
--- include/common/mavlink.h
+++ include/common/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Friday, August 5 2011, 07:37 UTC
+ *	 Generated on Friday, August 5 2011, 08:13 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
