401e5b002e99cb25083203244aecba38845590c1
lm <mail@qgroundcontrol.org>
2011-08-12 22:49:07 +0200

    Updated MAVLink

--- include/mavlink_options.h
+++ include/mavlink_options.h
@@ -11,15 +11,6 @@ extern "C" {
 #ifndef _ML_OPTIONS_H_
 #define _ML_OPTIONS_H_
 
-/**
- * Check if user read this file
- * To stop this error, #define MAVLINK_CONFIGURED
- */
-	
-//#ifndef MAVLINK_CONFIGURED
-//#error Please read mavlink_options.h in the MAVLink include folder and then #define MAVLINK_CONFIGURED
-//#endif
-	
 
 /**
  *
@@ -72,10 +63,8 @@ extern "C" {
  *  do not define this sybol, then define this symbol in all other files before
  *  including mavlink.h
  *
- *  Use #define MAVLINK_NO_DATA to disable data auto-inclusion and then include
- *  #include "mavlink_data.h" AFTER #include "mavlink.h"
- *
  */
+//#define MAVLINK_NO_DATA
 #ifdef MAVLINK_NO_DATA
   #undef MAVLINK_DATA
 #else
@@ -105,6 +94,39 @@ extern "C" {
 #define MAVLINK_CONST_READ( a ) a
 #endif
 
+
+/**
+ *
+ *  Convience functions. These are all in one send functions that are very
+ *  easy to use. Just define the symbol MAVLINK_USE_CONVENIENCE_FUNCTIONS.
+ *  These functions also support a buffer check, to ensure there is enough
+ *  space in your comm buffer that the function would not block - it could
+ *  also be used as the basis of a MUTEX. This is implemented in the send
+ *  function as a macro with two arguments, first the comm chan number and
+ *  the message length in the form 
+ *  MAVLINK_BUFFER_CHECK_START( chan, MAVLINK_MSG_ID_LEN )
+ *  followed by the function code and then
+ *  MAVLINK_BUFFER_CHECK_START
+ *  Note that there are no terminators on these statements to allow for
+ *  code nesting or other constructs. Default value for both is empty.
+ *  A sugested implementation is shown below and the symbols will be defined
+ *  only if they are not allready.
+ *
+ *  if ( serial_space( chan ) > len ) { // serial_space returns available space
+ *  ..... code that creates message
+ *  } 
+ *
+ *  #define MAVLINK_BUFFER_CHECK_START( c, l ) if ( serial_space( c ) > l ) {
+ *  #define MAVLINK_BUFFER_CHECK_END }
+ *
+ */
+//#define MAVLINK_USE_CONVENIENCE_FUNCTIONS
+#ifndef MAVLINK_BUFFER_CHECK_START
+#define MAVLINK_BUFFER_CHECK_START( c, l ) ;
+#endif
+#ifndef MAVLINK_BUFFER_CHECK_END
+#define MAVLINK_BUFFER_CHECK_END ;
+#endif
 
 #endif /* _ML_OPTIONS_H_ */
 
