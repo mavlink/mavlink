7d7ab533cb9c331c378f8896192f301c94c4fd2f
James Goppert <james.goppert@gmail.com>
2012-02-13 16:03:25 -0500

    Updated pymavlink.

--- update_pymavlink.sh
+++ update_pymavlink.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+pymavlinkTag=d04af92f82f28d1ed66c933f08ef0494ae6ff693
 rm -rf pymavlink
 git clone https://github.com/tridge/pymavlink.git
-cd pymavlink && rm -rf .git
+cd pymavlink && git checkout $pymavlinkTag && rm -rf .git
