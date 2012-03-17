95b601b0c9036739575d8a34392900b8c9ba266c
Andrew Tridgell <tridge@samba.org>
2012-03-17 14:38:35 +1100

    report unlocked time

--- pymavlink/examples/gpslock.py
+++ pymavlink/examples/gpslock.py
@@ -32,6 +32,8 @@ def lock_time(logfile):
     start_time = 0.0
     total_time = 0.0
     t = None
+    m = mlog.recv_match(type='GPS_RAW')
+    unlock_time = time.mktime(time.localtime(m._timestamp))
 
     while True:
         m = mlog.recv_match(type='GPS_RAW')
@@ -43,17 +45,20 @@ def lock_time(logfile):
             return total_time
         t = time.localtime(m._timestamp)
         if m.fix_type == 2 and not locked:
-            print("Locked at %s" % time.asctime(t))
+            print("Locked at %s after %.1f seconds" % (time.asctime(t),
+                                                       time.mktime(t) - unlock_time))
             locked = True
             start_time = time.mktime(t)
         elif m.fix_type == 1 and locked:
             print("Lost GPS lock at %s" % time.asctime(t))
             locked = False
             total_time += time.mktime(t) - start_time
+            unlock_time = time.mktime(t)
         elif m.fix_type == 0 and locked:
             print("Lost protocol lock at %s" % time.asctime(t))
             locked = False
             total_time += time.mktime(t) - start_time
+            unlock_time = time.mktime(t)
     return total_time
 
 total = 0.0
