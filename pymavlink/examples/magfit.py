5f6a2ed648283f07ff638bf32837c156bfeb58b7
Andrew Tridgell <tridge@samba.org>
2012-03-28 21:16:31 +1100

    magfit: added --noise option

--- pymavlink/examples/magfit.py
+++ pymavlink/examples/magfit.py
@@ -13,6 +13,7 @@ from optparse import OptionParser
 parser = OptionParser("magfit.py [options]")
 parser.add_option("--no-timestamps",dest="notimestamps", action='store_true', help="Log doesn't have timestamps")
 parser.add_option("--condition",dest="condition", default=None, help="select packets by condition")
+parser.add_option("--noise", type='float', default=0, help="noise to add")
 parser.add_option("--mav10", action='store_true', default=False, help="Use MAVLink protocol 1.0")
 
 (opts, args) = parser.parse_args()
@@ -20,17 +21,25 @@ parser.add_option("--mav10", action='store_true', default=False, help="Use MAVLi
 if opts.mav10:
     os.environ['MAVLINK10'] = '1'
 import mavutil
+from rotmat import Vector3
 
 if len(args) < 1:
     print("Usage: magfit.py [options] <LOGFILE...>")
     sys.exit(1)
 
+def noise():
+    '''a noise vector'''
+    from random import gauss
+    v = Vector3(gauss(0, 1), gauss(0, 1), gauss(0, 1))
+    v.normalize()
+    return v * opts.noise
+
 def select_data(data):
     ret = []
     counts = {}
     for d in data:
-        x,y,z = d
-        key = "%u:%u:%u" % (x/20,y/20,z/20)
+        mag = d
+        key = "%u:%u:%u" % (mag.x/20,mag.y/20,mag.z/20)
         if key in counts:
             counts[key] += 1
         else:
@@ -40,10 +49,9 @@ def select_data(data):
     print(len(data), len(ret))
     return ret
 
-def radius(data, offsets):
+def radius(mag, offsets):
     '''return radius give data point and offsets'''
-    x,y,z = data
-    return math.sqrt((x+offsets[0])**2 + (y+offsets[1])**2 + (z+offsets[2])**2)
+    return (mag + offsets).length()
 
 def radius_cmp(a, b, offsets):
     '''return radius give data point and offsets'''
@@ -56,11 +64,12 @@ def radius_cmp(a, b, offsets):
 
 def sphere_error(p, data):
     from scipy import sqrt
-    xofs,yofs,zofs,r = p
+    x,y,z,r = p
+    ofs = Vector3(x,y,z)
     ret = []
     for d in data:
-        x,y,z = d
-        err = r - sqrt((x+xofs)**2 + (y+yofs)**2 + (z+zofs)**2)
+        mag = d
+        err = r - radius(mag, ofs)
         ret.append(err)
     return ret
 
@@ -72,7 +81,7 @@ def fit_data(data):
     p1, ier = optimize.leastsq(sphere_error, p0[:], args=(data))
     if not ier in [1, 2, 3, 4]:
         raise RuntimeError("Unable to find solution")
-    return p1
+    return (Vector3(p1[0], p1[1], p1[2]), p1[3])
 
 def magfit(logfile):
     '''find best magnetometer offset fit to a log file'''
@@ -83,10 +92,7 @@ def magfit(logfile):
     data = []
 
     last_t = 0
-
-    # get the current mag offsets
-    m = mlog.recv_match(type='SENSOR_OFFSETS',condition=opts.condition)
-    offsets = (m.mag_ofs_x, m.mag_ofs_y, m.mag_ofs_z)
+    offsets = Vector3(0,0,0)
 
     # now gather all the data
     while True:
@@ -95,39 +101,36 @@ def magfit(logfile):
             break
         if m.get_type() == "SENSOR_OFFSETS":
             # update current offsets
-            offsets = (m.mag_ofs_x, m.mag_ofs_y, m.mag_ofs_z)
+            offsets = Vector3(m.mag_ofs_x, m.mag_ofs_y, m.mag_ofs_z)
         if m.get_type() == "RAW_IMU":
+            mag = Vector3(m.xmag, m.ymag, m.zmag)
             # add data point after subtracting the current offsets
-            data.append((m.xmag - offsets[0], m.ymag - offsets[1], m.zmag - offsets[2]))
+            data.append(mag - offsets + noise())
 
     print("Extracted %u data points" % len(data))
-    print("Current offsets: ( %6.1f %6.1f %6.1f )" % (offsets[0], offsets[1], offsets[2]))
+    print("Current offsets: %s" % offsets)
 
     data = select_data(data)
 
     # do an initial fit with all data
-    ofs = fit_data(data)
-    offsets = ofs[0:3]
-    field_strength = ofs[3]
+    (offsets, field_strength) = fit_data(data)
 
     for count in range(3):
         # sort the data by the radius
         data.sort(lambda a,b : radius_cmp(a,b,offsets))
 
-        print("Fit %u    : ( %6.1f %6.1f %6.1f )  field_strength=%6.1f to %6.1f" % (
-            count, offsets[0], offsets[1], offsets[2],
+        print("Fit %u    : %s  field_strength=%6.1f to %6.1f" % (
+            count, offsets,
             radius(data[0], offsets), radius(data[-1], offsets)))
         
         # discard outliers, keep the middle 3/4
         data = data[len(data)/8:-len(data)/8]
 
         # fit again
-        ofs = fit_data(data)
-        offsets = ofs[0:3]
-        field_strength = ofs[3]
+        (offsets, field_strength) = fit_data(data)
 
-    print("Final    : ( %6.1f %6.1f %6.1f )  field_strength=%6.1f to %6.1f" % (
-        offsets[0], offsets[1], offsets[2],
+    print("Final    : %s  field_strength=%6.1f to %6.1f" % (
+        offsets,
         radius(data[0], offsets), radius(data[-1], offsets)))
 
 total = 0.0
