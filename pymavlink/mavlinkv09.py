dc75c66ff6f71603ac04620792aede81e1411b1e
Pat Hickey <pat@moreproductive.org>
2012-07-02 08:19:50 -0700

    regenerate mavlinkv09 and v10.py

--- pymavlink/mavlinkv09.py
+++ pymavlink/mavlinkv09.py
@@ -6,7 +6,7 @@ Generated from: ardupilotmega.xml,common.xml
 Note: this file has been auto-generated. DO NOT EDIT
 '''
 
-import struct, array, mavutil, time
+import struct, array, mavutil, time, json
 
 WIRE_PROTOCOL_VERSION = "0.9"
 
@@ -88,16 +88,15 @@ class MAVLink_message(object):
         ret = ret[0:-2] + '}'
         return ret            
 
-    def to_json(self):
-        ret = '{ "mavpackettype":"%s", ' % self._type
+    def to_dict(self):
+        d = dict({})
+        d['mavpackettype'] = self._type
         for a in self._fieldnames:
-            v = getattr(self, a)
-            if type(v) == int or type(v) == float:
-              ret += '"%s" : %s, ' % (a, v)
-            else:
-              ret += '"%s" : "%s", ' % (a, v)
-        ret = ret[0:-2] + '}'
-        return ret
+          d[a] = getattr(self, a)
+        return d
+
+    def to_json(self):
+        return json.dumps(self.to_dict)
 
     def pack(self, mav, crc_extra, payload):
         self._payload = payload
