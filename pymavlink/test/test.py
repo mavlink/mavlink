35ebc2f1bdfef160c31c284f1153029a9a77fe26
Lenna Peterson <arklenna@gmail.com>
2013-01-15 19:33:01 -0500

    Improved test architecture.
    * Capture stdout
    * Global function for calling command line scripts
    * pep8 & camelcase

--- pymavlink/test/test.py
+++ pymavlink/test/test.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 
-import unittest, os, inspect
+import inspect
+import os
 import subprocess
+import unittest
+
 from pymavlink import *
 
-rootPath = os.path.realpath(os.path.join(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]),'..'))
-exPath = os.path.join(rootPath, 'examples')
-dataPath = os.path.join(rootPath, 'test', 'data')
+
+# Real, absolute path of directory above the script location
+root_path = os.path.realpath(os.path.join(
+    os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]),
+    os.path.pardir))
+ex_path = os.path.join(root_path, 'examples')
+data_path = os.path.join(root_path, 'test', 'data')
+
+
+def run_cmd(cmd, input=None):
+    """
+    cmd (iterable): program and arguments to execute
+    input (string): input for STDIN
+    returns 3-tuple: (returncode, stdout, stderr)
+    """
+    PIPE = subprocess.PIPE
+    proc = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate(input)
+    return proc.returncode, out, err
+
 
 class Test(unittest.TestCase):
 
@@ -17,10 +37,9 @@ class Test(unittest.TestCase):
         print 'test simple'
 
     def test_logdumpv09(self):
-        prog = os.path.join(exPath,'mavlogdump.py')
-        data = os.path.join(dataPath,'mav09.log')
-        try:
-            log = subprocess.check_call([prog,data])
-            #log = subprocess.check_call("exit 1", shell=True, stdout=subprocess.STDOUT)
-        except subprocess.CalledProcessError:
-            print log
+        prog = os.path.join(ex_path, 'mavlogdump.py')
+        data = os.path.join(data_path, 'mav09.log')
+        input = None
+        retcode, out, err = run_cmd([prog, data], input)
+        if retcode != 0:
+            print err
