efc4839eaa224fb99ac753a1fcfb0e63b451cf08
Bryant <bwmairs@ucsc.edu>
2013-02-20 11:04:32 -0800

    generate.py is now Python 3 compatible. XML validation doesn't work in Python3.

--- pymavlink/generator/mavgen.py
+++ pymavlink/generator/mavgen.py
@@ -20,9 +20,14 @@ import mavgen_cs
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
 
-from genxmlif import GenXmlIfError
-from minixsv import pyxsval 
-
+try:
+    from genxmlif import GenXmlIfError
+    from minixsv import pyxsval
+    performValidation = True
+except:
+    print("Unable to load XML validator libraries. XML validation will not be performed")
+    performValidation = False
+    
 # XSD schema file
 schemaFile = os.path.join(os.path.dirname(os.path.realpath(__file__)), "mavschema.xsd")
 
@@ -35,8 +40,11 @@ def mavgen(opts, args) :
     xml = []
 
     for fname in args:
-        print("Validating %s" % fname)
-        mavgen_validate(fname, schemaFile, opts.error_limit);
+        if performValidation:
+            print("Validating %s" % fname)
+            mavgen_validate(fname, schemaFile, opts.error_limit);
+        else:
+            print("Validation skipped.")
 
         print("Parsing %s" % fname)
         xml.append(mavparse.MAVXML(fname, opts.wire_protocol))
@@ -47,8 +55,11 @@ def mavgen(opts, args) :
             fname = os.path.join(os.path.dirname(x.filename), i)
 
             ## Validate XML file with XSD file
-            print("Validating %s" % fname)
-            mavgen_validate(fname, schemaFile, opts.error_limit);
+            if performValidation:
+                print("Validating %s" % fname)
+                mavgen_validate(fname, schemaFile, opts.error_limit);
+            else:
+                print("Validation skipped.")
 
             ## Parsing
             print("Parsing %s" % fname)
