bad67a68e03a3b60b2ca4ef231d901425da228eb
Andrew Tridgell <tridge@samba.org>
2013-06-16 11:52:20 +1000

    pymavlink: added initial setup.py for distutils

---
+++ pymavlink/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup, Extension
+ 
+setup (name = 'pymavlink',
+       version = '1.0',
+       description = 'Python mavlink code',
+       packages = ['pymavlink', 'pymavlink.generator'],
+       scripts = [ 'pymavlink/tools/magfit_delta.py', 'pymavlink/tools/mavextract.py',
+                   'pymavlink/tools/mavgraph.py', 'pymavlink/tools/mavparmdiff.py',
+                   'pymavlink/tools/mavtogpx.py', 'pymavlink/tools/magfit_gps.py',
+                   'pymavlink/tools/mavflightmodes.py', 'pymavlink/tools/mavlogdump.py',
+                   'pymavlink/tools/mavparms.py', 'pymavlink/tools/magfit_motors.py',
+                   'pymavlink/tools/mavflighttime.py', 'pymavlink/tools/mavloss.py',
+                   'pymavlink/tools/mavplayback.py', 'pymavlink/tools/magfit.py',
+                   'pymavlink/tools/mavgpslock.py',
+                   'pymavlink/tools/mavmission.py',
+                   'pymavlink/tools/mavsigloss.py' ]
+       )
