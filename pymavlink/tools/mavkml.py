c55a30df4e6e53cd784c7d4d254adfbeb6695e2d
Thomas Gubler <thomasgubler@gmail.com>
2014-01-18 19:12:38 +0100

    kml: gps as linestring works

--- pymavlink/tools/mavkml.py
+++ pymavlink/tools/mavkml.py
@@ -13,17 +13,33 @@ import time
 import re
 
 #xxx: also global position
-gps_fields = ['GPS.Lat', 'GPS.Lon']
-gps_data = {gps_fields[0]: None, gps_fields[1]: None}
+gps_fields = ['GPS.Lon', 'GPS.Lat', 'GPS.Alt'] #kml order is lon, lat
+gps_data = [None for n in gps_fields]
 
 kml = simplekml.Kml()
+kml_linestrings = [kml.newlinestring(name='GPS', altitudemode='absolute')]
+kml_linestyles = [simplekml.LineStyle(color=simplekml.Color.blue)]
 
 #xxx altitude
-def add_kml(data):
+def add_to_linestring(data, kml_linestring):
     '''add a point to the kml file'''
     global kml
-    v = data.values()[0:2]
-    kml.newpoint(coords=[v])
+#     v = [data['GPS.Lat'], data['GPS.Lon'], data['GPS.Alt']]
+# #     print v
+#     pnt = kml.newpoint(coords=(v))
+#     pnt.altitudemode = 'absolute'
+    kml_linestring.coords.addcoordinates([gps_data])
+
+def style_kml(linestrings, linestyles, extrude):
+    '''applies the styles to the linestrings'''
+    assert len(linestrings) == len(linestyles)
+    
+    if extrude:
+        for l in linestrings:
+            l.extrude = 1   
+
+    for i,v in enumerate(linestrings):
+        v.style.linestyle = linestyles[i]
 
 def save_kml(filename):
     '''saves the kml file'''
@@ -49,16 +65,16 @@ def add_data(t, msg, vars):
         if v is None:
             continue
         
-        gps_data[f] = v
+        gps_data[gps_fields.index(f)] = v
     
     #check if we have a full gps measurement
     gps_measurement_ready = True;
-    for n,o in gps_data.items():
-        if o == None:
+    for v in gps_data:
+        if v == None:
             gps_measurement_ready = False
     if gps_measurement_ready:
-        add_kml(gps_data)
-        gps_data = {n: None for n in gps_data} #reset gps_data
+        add_to_linestring(gps_data, kml_linestrings[0])
+        gps_data = [None for n in gps_data] #reset gps_data
         
             
 
@@ -79,7 +95,9 @@ if __name__ == '__main__':
     parser = OptionParser("mavkml.py [options] <filename>")
     parser.add_option("--no-timestamps",dest="notimestamps", action='store_true', help="Log doesn't have timestamps")
     parser.add_option("--condition",dest="condition", default=None, help="select packets by a condition")
-    parser.add_option("-o",dest="filename_out", default="mavkml.kml", help="ouput filename")
+    parser.add_option("--a",dest="aoff", default=0, help="Altitude offset for paths that go through the ground in google earth")
+    parser.add_option("-o",dest="filename_out", default="mavkml.kml", help="output filename")
+    parser.add_option("-e",dest="extrude", action='store_true', help="extrude paths to ground")
     
     
     (opts, args) = parser.parse_args()
@@ -110,5 +128,6 @@ if __name__ == '__main__':
     for fi in range(0, len(filenames)):
         f = filenames[fi]
         process_file(f)
-        
+    
+    style_kml(kml_linestrings, kml_linestyles, opts.extrude)
     save_kml(opts.filename_out)
