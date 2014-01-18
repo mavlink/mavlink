abd21830f524212196fa5ca32fe18d1e0895437d
Thomas Gubler <thomasgubler@gmail.com>
2014-01-18 22:58:28 +0100

    kml: colored plots based on state working, not using multigeometry anymore (does not allow individual styles for sum-elements)

--- pymavlink/tools/mavkml.py
+++ pymavlink/tools/mavkml.py
@@ -12,47 +12,34 @@ from pymavlink import mavutil
 import time
 import re
 
-#xxx: also global position
-gps_fields = ['GPS.Lon', 'GPS.Lat', 'GPS.Alt'] #kml order is lon, lat
-gps_data = [None for n in gps_fields]
+mainstate_field = 'STAT.MainState'
+position_field_types = ['Lon', 'Lat', 'Alt'] #kml order is lon, lat
+
+colors = [simplekml.Color.red, simplekml.Color.green, simplekml.Color.blue, simplekml.Color.violet]
 
 kml = simplekml.Kml()
-kml_linestrings = [kml.newlinestring(name='GPS', altitudemode='absolute')]
-kml_linestyles = [simplekml.LineStyle(color=simplekml.Color.blue)]
+kml_linestrings = []
 
-#xxx altitude
-def add_to_linestring(data, kml_linestring):
+def add_to_linestring(position_data, kml_linestring):
     '''add a point to the kml file'''
     global kml
-#     v = [data['GPS.Lat'], data['GPS.Lon'], data['GPS.Alt']]
-# #     print v
-#     pnt = kml.newpoint(coords=(v))
-#     pnt.altitudemode = 'absolute'
-    kml_linestring.coords.addcoordinates([gps_data])
 
-def style_kml(linestrings, linestyles, extrude):
-    '''applies the styles to the linestrings'''
-    assert len(linestrings) == len(linestyles)
+    #add altitude offset
+    position_data[2] += float(opts.aoff)
+    kml_linestring.coords.addcoordinates([position_data])
     
-    if extrude:
-        for l in linestrings:
-            l.extrude = 1   
-
-    for i,v in enumerate(linestrings):
-        v.style.linestyle = linestyles[i]
-
 def save_kml(filename):
     '''saves the kml file'''
     global kml
     kml.save(filename)
+    print "KML written to " + filename
 
 #test
 # exportKML([(47.5, 8.), ((48., 9.))], "test.kml")
 
-def add_data(t, msg, vars):
+def add_data(t, msg, vars, fields, field_types):
     '''add some data'''
-    global gps_data
-    
+        
     mtype = msg.get_type()
     if mtype not in msg_types:
         return
@@ -65,39 +52,72 @@ def add_data(t, msg, vars):
         if v is None:
             continue
         
-        gps_data[gps_fields.index(f)] = v
+        # Check if we have position or state information
+        if f == mainstate_field:
+            # Handle main state information
+            if v != add_data.mainstate_current and add_data.mainstate_current >= 0: # add_data.mainstate_current >= 0 : avoid starting a new linestring when mainstate comes after the first position data in the log
+                add_data.new_linestring = True
+            add_data.mainstate_current = v
+        else:
+            # Handle position information
+            add_data.position_data[i] = v # make sure lon, lat, alt is saved in the correct order in position_data (the same as in position_field_types)
     
     #check if we have a full gps measurement
     gps_measurement_ready = True;
-    for v in gps_data:
+    for v in add_data.position_data:
         if v == None:
             gps_measurement_ready = False
+            
     if gps_measurement_ready:
-        add_to_linestring(gps_data, kml_linestrings[0])
-        gps_data = [None for n in gps_data] #reset gps_data
+        #if new line string is needed (because of state change): add previous linestring to kml_linestrings list, add a new linestring to the kml multigeometry and append to the new linestring
+        #else: append to current linestring
+        if add_data.new_linestring:
+            if add_data.current_kml_linestring != None:
+                kml_linestrings.append(add_data.current_kml_linestring)
+            
+            add_data.current_kml_linestring = kml.newlinestring(name=opts.source  + ":" + str(add_data.mainstate_current), altitudemode='absolute')
+            
+            #set rendering options
+            if opts.extrude:
+                add_data.current_kml_linestring.extrude = 1
+            add_data.current_kml_linestring.style.linestyle.color =  colors[max([add_data.mainstate_current, 0])]
+            
+            add_data.new_linestring = False
+        
+        add_to_linestring(add_data.position_data, add_data.current_kml_linestring)
+        
+        #reset position_data
+        add_data.position_data = [None for n in position_field_types] 
         
             
 
-def process_file(filename):
+def process_file(filename, fields, field_types):
     '''process one file'''
     print("Processing %s" % filename)
     mlog = mavutil.mavlink_connection(filename, notimestamps=opts.notimestamps)
     vars = {}
-     
+    position_data = [None for n in position_field_types]
+    mainstate_current = -1
+    add_data.new_linestring = True
+    add_data.mainstate_current = -1
+    add_data.current_kml_linestring = None
+    add_data.position_data = [None for n in position_field_types]
+    
     while True:
         msg = mlog.recv_match(opts.condition)
         if msg is None: break
         tdays = (msg._timestamp - time.timezone) / (24 * 60 * 60)
         tdays += 719163 # pylab wants it since 0001-01-01
-        add_data(tdays, msg, mlog.messages)
+        add_data(tdays, msg, mlog.messages, fields, field_types)
 
 if __name__ == '__main__':
     parser = OptionParser("mavkml.py [options] <filename>")
     parser.add_option("--no-timestamps",dest="notimestamps", action='store_true', help="Log doesn't have timestamps")
     parser.add_option("--condition",dest="condition", default=None, help="select packets by a condition")
-    parser.add_option("--a",dest="aoff", default=0, help="Altitude offset for paths that go through the ground in google earth")
-    parser.add_option("-o",dest="filename_out", default="mavkml.kml", help="output filename")
-    parser.add_option("-e",dest="extrude", action='store_true', help="extrude paths to ground")
+    parser.add_option("--aoff",dest="aoff", default=0., help="Altitude offset for paths that go through the ground in google earth")
+    parser.add_option("-o", "--output",dest="filename_out", default="mavkml.kml", help="output filename")
+    parser.add_option("-s", "--source", dest="source", default="GPOS", help="write report to FILE", metavar="FILE")
+    parser.add_option("-e", "--extrude", dest="extrude", default=False, action='store_true', help="extrude paths to ground")
     
     
     (opts, args) = parser.parse_args()
@@ -111,8 +131,11 @@ if __name__ == '__main__':
         if os.path.exists(f):
             filenames.append(f)
         
-    fields = gps_fields
+    #init fields and field_types lists
+    fields = [opts.source + "." + s for s in position_field_types]
+    fields.append(mainstate_field)
     field_types = []
+    
     msg_types = set()
     re_caps = re.compile('[A-Z_][A-Z0-9_]+')
     
@@ -127,7 +150,6 @@ if __name__ == '__main__':
      
     for fi in range(0, len(filenames)):
         f = filenames[fi]
-        process_file(f)
+        process_file(f, fields, field_types)
     
-    style_kml(kml_linestrings, kml_linestyles, opts.extrude)
     save_kml(opts.filename_out)
