mavlink/mavlink

commit 9ae9536fbcbeb88b3ba45b2c44ff5dc9e3e1cf5f

Corrected Python code so that it can be included easily. This involved a switch to relative imports for most Python library code. There are also fixes for Python3 compatibility. Now mavlogdump.py and dialect library generation work correctly under Python3.3 & 2.7. Updated the README with more details on the library and its requirements, installation, and usage.

pymavlink/generator/mavgen.py
@@ -83,22 +83,19 @@ def mavgen(opts, args) :
     # Convert language option to lowercase and validate
     opts.language = opts.language.lower()
     if opts.language == 'python':
-        import mavgen_python
+        from . import mavgen_python
         mavgen_python.generate(opts.output, xml)
     elif opts.language == 'c':
-        try:
-            import mavgen_c
-        except Exception:
-            from pymavlink.generator import mavgen_c
+        from . import mavgen_c
         mavgen_c.generate(opts.output, xml)
     elif opts.language == 'wlua':
-        import mavgen_wlua
+        from . import mavgen_wlua
         mavgen_wlua.generate(opts.output, xml)
     elif opts.language == 'cs':
-        import mavgen_cs
+        from . import mavgen_cs
         mavgen_cs.generate(opts.output, xml)
     elif opts.language == 'javascript':
-        import mavgen_javascript
+        from . import mavgen_javascript
         mavgen_javascript.generate(opts.output, xml)
     else:
         print("Unsupported language %s" % opts.language)
@@ -127,13 +124,18 @@ def mavgen_python_dialect(dialect, wire_protocol):
         if not os.path.exists(xml):
             xml = os.path.join(mdef, 'v1.0', dialect + '.xml')
     opts = Opts(wire_protocol, py)
-    import StringIO
-
-    # throw away stdout while generating
-    stdout_saved = sys.stdout
-    sys.stdout = StringIO.StringIO()
+    
+    # Obtain StringIO module dependent on current Python version
     try:
-        xml = os.path.relpath(xml)
+        from io import StringIO
+    except Exception:
+        from StringIO import StringIO
+
+    # Throw away stdout while generating
+    stdout_saved = sys.stdout
+    sys.stdout = StringIO()
+    try:
+        xml = os.path.abspath(xml)
         mavgen( opts, [xml] )
     except Exception:
         sys.stdout = stdout_saved

mavgenerate.py
@@ -41,7 +41,7 @@ except ImportError as ex:
     del tkMessageBox
 
 sys.path.append(os.path.join('pymavlink','generator'))
-from mavgen import *
+from pymavlink.generator import mavgen
 
 DEBUG = False
 title = "MAVLink Generator"

pymavlink/generator/mavgen_python.py
@@ -7,7 +7,8 @@ Released under GNU GPL version 3 or later
 '''
 
 import sys, textwrap, os
-import mavparse, mavtemplate
+from . import mavparse
+from . import mavtemplate
 
 t = mavtemplate.MAVTemplate()
 

pymavlink/generator/mavparse.py
@@ -282,7 +282,7 @@ class MAVXML(object):
 def message_checksum(msg):
     '''calculate a 8-bit checksum of the key fields of a message, so we
        can detect incompatible XML changes'''
-    from mavcrc import x25crc
+    from .mavcrc import x25crc
     crc = x25crc(msg.name + ' ')
     for f in msg.ordered_fields:
         crc.accumulate(f.type + ' ')

pymavlink/generator/mavtemplate.py
@@ -6,7 +6,7 @@ Copyright Andrew Tridgell 2011
 Released under GNU GPL version 3 or later
 '''
 
-from mavparse import MAVParseError
+from .mavparse import MAVParseError
 
 class MAVTemplate(object):
     '''simple templating system'''

pymavlink/mavutil.py
@@ -11,11 +11,11 @@ import socket, math, struct, time, os, fnmatch, array, sys, errno
 # adding these extra imports allows pymavlink to be used directly with pyinstaller
 # without having complex spec files
 import json
-from pymavlink.dialects.v10 import ardupilotmega
+from .dialects.v10 import ardupilotmega
 
 # these imports allow for mavgraph and mavlogdump to use maths expressions more easily
 from math import *
-from mavextra import *
+from .mavextra import *
 
 '''
 Support having a $HOME/.pymavlink/mavextra.py for extra graphing functions
@@ -26,7 +26,7 @@ if home is not None:
     if os.path.exists(extra):
         import imp
         mavuser = imp.load_source('pymavlink.mavuser', extra)
-        from pymavlink.mavuser import *
+        from .mavuser import *
 
 mavlink = None
 
@@ -71,7 +71,7 @@ def set_dialect(dialect):
     For example, set_dialect("ardupilotmega")
     '''
     global mavlink, current_dialect
-    from generator import mavparse
+    from .generator import mavparse
     if mavlink is None or mavlink.WIRE_PROTOCOL_VERSION == "1.0" or not 'MAVLINK09' in os.environ:
         wire_protocol = mavparse.PROTOCOL_1_0
         modname = "pymavlink.dialects.v10." + dialect
@@ -83,7 +83,7 @@ def set_dialect(dialect):
         mod = __import__(modname)
     except Exception:
         # auto-generate the dialect module
-        from generator.mavgen import mavgen_python_dialect
+        from .generator.mavgen import mavgen_python_dialect
         mavgen_python_dialect(dialect, wire_protocol)
         mod = __import__(modname)
     components = modname.split('.')
@@ -144,15 +144,23 @@ class mavfile(object):
     def auto_mavlink_version(self, buf):
         '''auto-switch mavlink protocol version'''
         global mavlink
-        if len(buf) == 0:
-            return
-        if not ord(buf[0]) in [ 85, 254 ]:
+        
+        # Make sure we have the right datatype for buf[0]
+        intbuf = buf[0]
+        if type(intbuf) is str:
+            intbuf = ord(buf[0])
+        
+        # Make sure that the MAVLink message starts with a valid start byte
+        if not intbuf in [ 85, 254 ]:
             return
         self.first_byte = False
-        if self.WIRE_PROTOCOL_VERSION == "0.9" and ord(buf[0]) == 254:
+        
+        # If it's 254, then it should be MAVLink version 1.0.
+        if self.WIRE_PROTOCOL_VERSION == "0.9" and intbuf == 254:
             self.WIRE_PROTOCOL_VERSION = "1.0"
             set_dialect(current_dialect)
-        elif self.WIRE_PROTOCOL_VERSION == "1.0" and ord(buf[0]) == 85:
+        # Otherwise if it's 85, then it should be version 0.9
+        elif self.WIRE_PROTOCOL_VERSION == "1.0" and intbuf == 85:
             self.WIRE_PROTOCOL_VERSION = "0.9"
             set_dialect(current_dialect)
             os.environ['MAVLINK09'] = '1'
@@ -957,7 +965,7 @@ def mavlink_connection(device, baud=115200, source_system=255,
 
     if device.lower().endswith('.bin'):
         # support dataflash logs
-        from pymavlink import DFReader
+        from . import DFReader
         m = DFReader.DFReader_binary(device, zero_time_base=zero_time_base)
         global mavfile_global
         mavfile_global = m
@@ -965,7 +973,7 @@ def mavlink_connection(device, baud=115200, source_system=255,
 
     if device.endswith('.log'):
         # support dataflash text logs
-        from pymavlink import DFReader
+        from . import DFReader
         if DFReader.DFReader_is_text_log(device):
             global mavfile_global
             m = DFReader.DFReader_text(device, zero_time_base=zero_time_base)

commit 29aac4363bc1d475d8d52c0acf3a3f8914344340

Fixed bug introduced in 9ae9536 involving the io library. @see #173

pymavlink/generator/mavgen.py
@@ -35,7 +35,7 @@ def mavgen(opts, args) :
 
     for fname in args:
         if performValidation:
-            print("Validating %s" % fname)
+            print("Validating {}".format(fname))
             mavgen_validate(fname, schemaFile, opts.error_limit);
         else:
             print("Validation skipped for %s." % fname)
@@ -126,10 +126,11 @@ def mavgen_python_dialect(dialect, wire_protocol):
     opts = Opts(wire_protocol, py)
     
     # Obtain StringIO module dependent on current Python version
+    # For Python2.7, default to the StringIO version, for Python3, use the io library.
     try:
-        from io import StringIO
-    except Exception:
         from StringIO import StringIO
+    except Exception:
+        from io import StringIO
 
     # Throw away stdout while generating
     stdout_saved = sys.stdout

pymavlink/tools/mavlogdump.py
@@ -7,11 +7,16 @@ of a series of MAVLink packets, each with a 64 bit timestamp
 header. The timestamp is in microseconds since 1970 (unix epoch)
 '''
 
-import sys, time, os, struct, json
+import sys, time, os, struct, json, inspect, datetime
 
 from optparse import OptionParser
-parser = OptionParser("mavlogdump.py [options] <LOGFILE>")
+from pymavlink import mavutil
 
+# Get the current year. We use this to correct invalid byte-ordering in MAVLink timestamps
+current_year = datetime.datetime.now().year
+
+# Set up some command-line options.
+parser = OptionParser("mavlogdump.py [options] <LOGFILE>")
 parser.add_option("--no-timestamps",dest="notimestamps", action='store_true', help="Log doesn't have timestamps")
 parser.add_option("--planner",dest="planner", action='store_true', help="use planner file format")
 parser.add_option("--robust",dest="robust", action='store_true', help="Enable robust parsing (skip over bad data)")
@@ -27,15 +32,12 @@ parser.add_option("--dialect",  default="ardupilotmega", help="MAVLink dialect")
 parser.add_option("--zero-time-base",  action='store_true', help="use Z time base for DF logs")
 (opts, args) = parser.parse_args()
 
-import inspect
-
-from pymavlink import mavutil
-
-
+# Print the help if the user didn't specify any arguments
 if len(args) < 1:
     parser.print_help()
     sys.exit(1)
 
+# Open a MAVLink connection on the provided file
 filename = args[0]
 mlog = mavutil.mavlink_connection(filename, planner_format=opts.planner,
                                   notimestamps=opts.notimestamps,
@@ -109,8 +111,14 @@ while True:
     if m.get_type() == 'BAD_DATA' and m.reason == "Bad prefix":
         continue
 
-    # Grab the timestamp.
+    # Grab the timestamp. Older versions of QGC stored timestamps in the wrong byte-order,
+    # so try a different byte ordering if the year for this timestamp is in the future.
     timestamp = getattr(m, '_timestamp', None)
+    try:
+        if time.localtime(timestamp).tm_year > current_year:
+            timestamp = struct.unpack('>Q', struct.pack('<Q', int(timestamp*1e6)))[0] / 1e6
+    except:
+        timestamp = struct.unpack('>Q', struct.pack('<Q', int(timestamp*1e6)))[0] / 1e6
 
     # If we're just logging, pack in the timestamp and data into the output file.
     if output:

pymavlink/tools/mavloss.py
@@ -12,6 +12,7 @@ parser.add_option("--no-timestamps",dest="notimestamps", action='store_true', he
 parser.add_option("--planner",dest="planner", action='store_true', help="use planner file format")
 parser.add_option("--robust",dest="robust", action='store_true', help="Enable robust parsing (skip over bad data)")
 parser.add_option("--condition", default=None, help="condition for packets")
+parser.add_option("--dialect",  default="ardupilotmega", help="MAVLink dialect")
 
 (opts, args) = parser.parse_args()
 
@@ -27,7 +28,8 @@ def mavloss(logfile):
     mlog = mavutil.mavlink_connection(filename,
                                       planner_format=opts.planner,
                                       notimestamps=opts.notimestamps,
-                                      robust_parsing=opts.robust)
+                                      robust_parsing=opts.robust,
+                                      dialect=opts.dialect)
 
     m = mlog.recv_match(condition=opts.condition)
 