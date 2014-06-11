f99e24543984fa09c25e5a5d0822707732240bd7
Bryant Mairs <bwmairs@gmail.com>
2014-06-11 16:28:11 -0700

    inttypes.h is not necessary, only stdint.h is, so just include that. This fixes support for some microcontrollers that don't have inttypes.h in their standard library. Only issue is that Microsoft's Visual Studio/C++ < 2010 don't have stdint.h, but I don't think that's a supported platform at this point.

--- pymavlink/generator/C/include_v1.0/mavlink_helpers.h
+++ pymavlink/generator/C/include_v1.0/mavlink_helpers.h
@@ -214,7 +214,7 @@ MAVLINK_HELPER void mavlink_update_checksum(mavlink_message_t* msg, uint8_t c)
  * A typical use scenario of this function call is:
  *
  * @code
- * #include <inttypes.h> // For fixed-width uint8_t type
+ * #include <stdint.h> // For fixed-width uint8_t type
  *
  * mavlink_message_t msg;
  * int chan = 0;
