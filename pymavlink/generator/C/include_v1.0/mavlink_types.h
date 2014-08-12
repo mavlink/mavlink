beeb7a923469926a86702127176e11861fc92caa
Liam Staskawicz <liam@stask.net>
2014-08-11 20:55:04 -0700

    mavlink_types.h: fix a few more incorrect bitwise preprocessor ops

--- pymavlink/generator/C/include_v1.0/mavlink_types.h
+++ pymavlink/generator/C/include_v1.0/mavlink_types.h
@@ -23,7 +23,7 @@
 #define MAVLINK_MSG_ID_EXTENDED_MESSAGE 255
 #define MAVLINK_EXTENDED_HEADER_LEN 14
 
-#if (defined _MSC_VER) | ((defined __APPLE__) & (defined __MACH__)) | (defined __linux__)
+#if (defined _MSC_VER) || ((defined __APPLE__) && (defined __MACH__)) || (defined __linux__)
   /* full fledged 32bit++ OS */
   #define MAVLINK_MAX_EXTENDED_PACKET_LEN 65507
 #else
