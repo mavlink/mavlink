mavlink/mavlink

commit d41fe510d8cdb61f090f9c2aa11027597a7c6a40

Allow compiling on Visual Studio 2012 and earlier.

pymavlink/generator/C/include_v1.0/mavlink_types.h
@@ -1,7 +1,12 @@
 #ifndef MAVLINK_TYPES_H_
 #define MAVLINK_TYPES_H_
 
+// Visual Studio versions before 2013 don't conform to C99.
+#if (defined _MSC_VER) & (_MSC_VER < 1800)
+#include <stdint.h>
+#else
 #include <inttypes.h>
+#endif
 
 #ifndef MAVLINK_MAX_PAYLOAD_LEN
 // it is possible to override this, but be careful!

commit 359a246199b1ebf162e77da3b9c1b2750bce8122

mavlink_types.h: fix typo introduced in d41fe510d8cd

pymavlink/generator/C/include_v1.0/mavlink_types.h
@@ -2,7 +2,7 @@
 #define MAVLINK_TYPES_H_
 
 // Visual Studio versions before 2013 don't conform to C99.
-#if (defined _MSC_VER) & (_MSC_VER < 1800)
+#if (defined _MSC_VER) && (_MSC_VER < 1800)
 #include <stdint.h>
 #else
 #include <inttypes.h>