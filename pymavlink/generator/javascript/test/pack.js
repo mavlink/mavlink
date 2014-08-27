e39493cce43c5b972d20ff0b73e3b0236d701a5f
Andreas Antener <antener_a@gmx.ch>
2014-08-28 01:33:00 +0200

    adding example test cases for the use with Long.js

--- pymavlink/generator/javascript/test/pack.js
+++ pymavlink/generator/javascript/test/pack.js
@@ -1,5 +1,57 @@
 var should = require('should');
 var jspack = require('../lib/node-jspack-int64/jspack.js').jspack;
+var Long = require('long');
+
+describe('Test long integration (examples)', function() {
+
+	// Demonstrating the use together with Long.js (https://github.com/dcodeIO/Long.js)
+	//
+	// Because jspack doesn't care about signedness, we have to tell Long.js what we want our selfs.
+	// Taking care of signedness in jspack would mean to introduce a dependency on something like
+	// Long.js.
+	//
+	// Test number u            228290380562207 (BE: 0x00, 0x00, 0xcf, 0xa0, 0xff, 0x09, 0xff, 0x1f)
+	//                                          (LE: 0x1f, 0xff, 0x09, 0xff, 0xa0, 0xcf, 0x00, 0x00)
+	// Test number s           -228290380562207 (BE: 0xff, 0xff, 0x30, 0x5f, 0x00, 0xf6, 0x00, 0xe1)
+	//                                          (LE: 0xe1, 0x00, 0xf6, 0x00, 0x5f, 0x30, 0xff, 0xff)
+
+	it('pack <Q', function() {
+		//var num = new Long(0xff09ff1f, 0x0000cfa0, true);
+		var num = Long.fromNumber(228290380562207, true);
+
+        var buf = jspack.Pack('<Q', [[num.getHighBitsUnsigned(), num.getLowBitsUnsigned()]]);
+        buf.should.be.eql([0x1f, 0xff, 0x09, 0xff, 0xa0, 0xcf, 0x00, 0x00]);
+    });
+
+    it('unpack <Q', function() {
+    	var num = new Long(0xff09ff1f, 0x0000cfa0, true);
+        var buf = jspack.Unpack('<Q', [0x1f, 0xff, 0x09, 0xff, 0xa0, 0xcf, 0x00, 0x00]);
+        buf.length.should.be.eql(1);
+        buf[0].length.should.be.eql(2);
+        var res = new Long(buf[0][1], buf[0][0], true);
+        num.equals(res).should.be.true;
+        res.toNumber().should.be.eql(228290380562207);
+    });
+
+    it('pack <q', function() {
+		//var num = new Long(0x00f600e1, 0xffff305f, true);
+		var num = Long.fromNumber(-228290380562207); // signed
+
+        var buf = jspack.Pack('<q', [[num.getHighBitsUnsigned(), num.getLowBitsUnsigned()]]);
+        buf.should.be.eql([0xe1, 0x00, 0xf6, 0x00, 0x5f, 0x30, 0xff, 0xff]);
+    });
+
+    it('unpack <q', function() {
+    	var num = new Long(0x00f600e1, 0xffff305f); // signed
+        var buf = jspack.Unpack('<q', [0xe1, 0x00, 0xf6, 0x00, 0x5f, 0x30, 0xff, 0xff]);
+        buf.length.should.be.eql(1);
+        buf[0].length.should.be.eql(2);
+        var res = new Long(buf[0][1], buf[0][0]);
+        num.equals(res).should.be.true;
+        res.toNumber().should.be.eql(-228290380562207);
+    });
+
+});
 
 describe('Test int64', function() {
 
