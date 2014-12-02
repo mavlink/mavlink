0ea0fb913cc95bb6c508526a6d56d971de33a781
Bryant <bwmairs@ucsc.edu>
2014-12-02 10:05:21 -0800

    Support new exception syntax.

--- pymavlink/fgFDM.py
+++ pymavlink/fgFDM.py
@@ -200,7 +200,7 @@ class fgFDM(object):
         '''parse a FD FDM buffer'''
         try:
             t = struct.unpack(self.pack_string, buf)
-        except struct.error, msg:
+        except struct.error as msg:
             raise fgFDMError('unable to parse - %s' % msg)
         self.values = list(t)
 
