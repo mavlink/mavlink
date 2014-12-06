ef80b7d30faed27cfd5d924a05bf55ca495e6ce5
Thomas Gubler <thomasgubler@gmail.com>
2014-12-06 10:27:23 +0100

    gen sh scripts: use mavgen.py from tools directory

--- pymavlink/generator/gen_js.sh
+++ pymavlink/generator/gen_js.sh
@@ -4,9 +4,9 @@ for protocol in 0.9 1.0; do
  for xml in ../../message_definitions/v$protocol/*.xml; do
      base=$(basename $xml .xml)
      mkdir -p javascript/implementations/mavlink_${base}_v${protocol}
-     
+
      # Generate MAVLink implementation
-     ./mavgen.py --lang=JavaScript --wire-protocol=$protocol --output=javascript/implementations/mavlink_${base}_v${protocol}/mavlink.js $xml || exit 1
+     ../tools/mavgen.py --lang=JavaScript --wire-protocol=$protocol --output=javascript/implementations/mavlink_${base}_v${protocol}/mavlink.js $xml || exit 1
 
      # Create package.json file
      cat >javascript/implementations/mavlink_${base}_v${protocol}/package.json <<EOF
