cbff01644a26f3204e99b64943045662dd5f02cb
Lorenz Meier <lm@inf.ethz.ch>
2014-12-29 11:26:58 +0100

    Travis CI: Force exit on error, fix repo URL

--- scripts/travis_update_generated_repos.sh
+++ scripts/travis_update_generated_repos.sh
@@ -12,6 +12,10 @@ then
 	exit 0
 fi
 
+# Fail on error
+set -e
+
+# Config for auto-building
 git remote rename origin upstream
 git config --global user.email "bot@pixhawk.org"
 git config --global user.name "PX4BuildBot"
@@ -20,7 +24,8 @@ echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
 # Build C library
 mkdir -p include/mavlink/v1.0
-cd include/mavlink/v1.0 && git clone https://github.com:mavlink/c_library.git
+cd include/mavlink/v1.0
+git clone https://github.com/mavlink/c_library.git
 ./scripts/update_c_library.sh
 
 # XXX add build steps for other libraries as well
