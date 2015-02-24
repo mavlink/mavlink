aa95e60018ad65fd5ea7ab3c0544c838b7f480bb
Bryant <bwmairs@ucsc.edu>
2015-02-24 09:54:15 -0800

    Add an option to ignore BAD_DATA messages to mavlogdump.py

--- pymavlink/tools/mavlogdump.py
+++ pymavlink/tools/mavlogdump.py
@@ -26,6 +26,7 @@ parser.add_argument("--types", default=None, help="types of messages (comma sepa
 parser.add_argument("--nottypes", default=None, help="types of messages not to include (comma separated)")
 parser.add_argument("--dialect", default="ardupilotmega", help="MAVLink dialect")
 parser.add_argument("--zero-time-base", action='store_true', help="use Z time base for DF logs")
+parser.add_argument("--no-bad-data", action='store_true', help="Don't output corrupted messages")
 parser.add_argument("log", metavar="LOG")
 args = parser.parse_args()
 
@@ -111,7 +112,9 @@ while True:
     if nottypes is not None and m.get_type() in nottypes:
         continue
 
-    if m.get_type() == 'BAD_DATA' and m.reason == "Bad prefix":
+    # Ignore BAD_DATA messages is the user requested or if they're because of a bad prefix. The
+    # latter case is normally because of a mismatched MAVLink version.
+    if m.get_type() == 'BAD_DATA' and (args.no_bad_data is True or m.reason == "Bad prefix"):
         continue
 
     # Grab the timestamp.
