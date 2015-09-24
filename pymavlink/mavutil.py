3ba366f6343e648f7781e4eab69e734eefbd5e32
Andrew Tridgell <andrew@tridgell.net>
2015-09-25 08:37:52 +1000

    pymavlink: improved mavmemlog
    setup messages hash

--- pymavlink/mavutil.py
+++ pymavlink/mavutil.py
@@ -997,6 +997,7 @@ class mavmemlog(mavfile):
         self._msgs = []
         self._index = 0
         self._count = 0
+        self.messages = {}
         while True:
             m = mav.recv_msg()
             if m is None:
@@ -1011,12 +1012,14 @@ class mavmemlog(mavfile):
         m = self._msgs[self._index]
         self._index += 1
         self.percent = (100.0 * self._index) / self._count
+        self.messages[m.get_type()] = m
         return m
 
     def rewind(self):
         '''rewind to start'''
         self._index = 0
         self.percent = 0
+        self.messages = {}
 
 class mavchildexec(mavfile):
     '''a MAVLink child processes reader/writer'''
