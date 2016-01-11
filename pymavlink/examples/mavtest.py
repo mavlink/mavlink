c24688263b59b077ea5ffd5348b54c30dceefd77
Andrew Tridgell <andrew@tridgell.net>
2016-01-11 18:06:17 +1100

    pymavlink: updated mavtest.py for mavlink2 testing

--- pymavlink/examples/mavtest.py
+++ pymavlink/examples/mavtest.py
@@ -2,7 +2,8 @@
 
 import sys, os
 
-from pymavlink import mavlinkv10 as mavlink
+from pymavlink.dialects.v10 import common as mavlink1
+from pymavlink.dialects.v20 import common as mavlink2
 
 class fifo(object):
     def __init__(self):
@@ -13,27 +14,42 @@ class fifo(object):
     def read(self):
         return self.buf.pop(0)
 
-# we will use a fifo as an encode/decode buffer
-f = fifo()
+def test_protocol(mavlink):
+    # we will use a fifo as an encode/decode buffer
+    f = fifo()
 
-# create a mavlink instance, which will do IO on file object 'f'
-mav = mavlink.MAVLink(f)
+    # create a mavlink instance, which will do IO on file object 'f'
+    mav = mavlink.MAVLink(f)
 
-# set the WP_RADIUS parameter on the MAV at the end of the link
-mav.param_set_send(7, 1, "WP_RADIUS", 101, mavlink.MAV_PARAM_TYPE_REAL32)
+    # set the WP_RADIUS parameter on the MAV at the end of the link
+    mav.param_set_send(7, 1, "WP_RADIUS", 101, mavlink.MAV_PARAM_TYPE_REAL32)
 
-# alternatively, produce a MAVLink_param_set object 
-# this can be sent via your own transport if you like
-m = mav.param_set_encode(7, 1, "WP_RADIUS", 101, mavlink.MAV_PARAM_TYPE_REAL32)
+    # alternatively, produce a MAVLink_param_set object 
+    # this can be sent via your own transport if you like
+    m = mav.param_set_encode(7, 1, "WP_RADIUS", 101, mavlink.MAV_PARAM_TYPE_REAL32)
 
-# get the encoded message as a buffer
-b = m.get_msgbuf()
+    m.pack(mav)
 
-# decode an incoming message
-m2 = mav.decode(b)
+    # get the encoded message as a buffer
+    b = m.get_msgbuf()
 
-# show what fields it has
-print("Got a message with id %u and fields %s" % (m2.get_msgId(), m2.get_fieldnames()))
+    bi=[]
+    for c in b:
+        bi.append(int(c))
+    print(bi)
 
-# print out the fields
-print(m2)
+    # decode an incoming message
+    m2 = mav.decode(b)
+
+    # show what fields it has
+    print("Got a message with id %u and fields %s" % (m2.get_msgId(), m2.get_fieldnames()))
+
+    # print out the fields
+    print(m2)
+
+
+print("Testing mavlink1")
+test_protocol(mavlink1)
+
+print("Testing mavlink2")
+test_protocol(mavlink2)
