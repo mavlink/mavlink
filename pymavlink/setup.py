80ddf565e97d44a04f7159629f8c5be6a3043722
Andrew Tridgell <andrew@tridgell.net>
2016-02-09 17:40:51 +1100

    pymavlink: raise version again
    pypi error

--- pymavlink/setup.py
+++ pymavlink/setup.py
@@ -11,7 +11,7 @@ except LookupError:
 from setuptools import setup, Extension
 import glob, os, shutil, fnmatch, platform
 
-version = '1.1.69'
+version = '1.1.70'
 
 from generator import mavgen, mavparse
 
