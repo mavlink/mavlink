75b98d187718275515c1d6be026c30fc00fd7b05
Andrew Tridgell <andrew@tridgell.net>
2016-02-25 07:45:19 +1100

    generator: change to 24 bit IDs and no dialect in header

--- pymavlink/generator/mavparse.py
+++ pymavlink/generator/mavparse.py
@@ -108,12 +108,11 @@ class MAVField(object):
 
 
 class MAVType(object):
-    def __init__(self, name, id, dialect, linenumber, description=''):
+    def __init__(self, name, id, linenumber, description=''):
         self.name = name
         self.name_lower = name.lower()
         self.linenumber = linenumber
         self.id = int(id)
-        self.dialect = dialect
         self.description = description
         self.fields = []
         self.fieldnames = []
@@ -159,7 +158,6 @@ class MAVXML(object):
         self.version = 2
         self.include = []
         self.wire_protocol_version = wire_protocol_version
-        self.dialect = 0
 
         # setup the protocol features for the requested protocol version
         if wire_protocol_version == PROTOCOL_0_9:
@@ -168,24 +166,21 @@ class MAVXML(object):
             self.little_endian = False
             self.crc_extra = False
             self.crc_struct = False
-            self.command_16bit = False
-            self.multi_dialect = False
+            self.command_24bit = False
         elif wire_protocol_version == PROTOCOL_1_0:
             self.protocol_marker = 0xFE
             self.sort_fields = True
             self.little_endian = True
             self.crc_extra = True
             self.crc_struct = False
-            self.command_16bit = False
-            self.multi_dialect = False
+            self.command_24bit = False
         elif wire_protocol_version == PROTOCOL_2_0:
             self.protocol_marker = 0xFD
             self.sort_fields = True
             self.little_endian = True
             self.crc_extra = True
             self.crc_struct = True
-            self.command_16bit = True
-            self.multi_dialect = True
+            self.command_24bit = True
         else:
             print("Unknown wire protocol version")
             print("Available versions are: %s %s" % (PROTOCOL_0_9, PROTOCOL_1_0, PROTOCOL_2_0))
@@ -205,7 +200,7 @@ class MAVXML(object):
             #print in_element
             if in_element == "mavlink.messages.message":
                 check_attrs(attrs, ['name', 'id'], 'message')
-                self.message.append(MAVType(attrs['name'], attrs['id'], self.dialect, p.CurrentLineNumber))
+                self.message.append(MAVType(attrs['name'], attrs['id'], p.CurrentLineNumber))
             elif in_element == "mavlink.messages.message.field":
                 check_attrs(attrs, ['name', 'type'], 'field')
                 if 'print_format' in attrs:
@@ -259,8 +254,6 @@ class MAVXML(object):
                 self.enum[-1].entry[-1].param[-1].description += data
             elif in_element == "mavlink.version":
                 self.version = int(data)
-            elif in_element == "mavlink.dialect":
-                self.dialect = int(data)
             elif in_element == "mavlink.include":
                 self.include.append(data)
 
@@ -323,10 +316,7 @@ class MAVXML(object):
                     m.num_fields, 64))
             m.crc_extra = message_checksum(m)
 
-            if self.multi_dialect:
-                key = (m.dialect,m.id)
-            else:
-                key = m.id
+            key = m.id
             self.message_crcs[key] = m.crc_extra
             self.message_lengths[key] = m.wire_length
             self.message_names[key] = m.name
@@ -397,13 +387,10 @@ def check_duplicates(xml):
     enummap = {}
     for x in xml:
         for m in x.message:
-            if x.multi_dialect:
-                key = (m.dialect,m.id)
-            else:
-                key = m.id
+            key = m.id
             if key in msgmap:
-                print("ERROR: Duplicate message id %u:%u for %s (%s:%u) also used by %s" % (
-                    m.dialect, m.id,
+                print("ERROR: Duplicate message id %u for %s (%s:%u) also used by %s" % (
+                    m.id,
                     m.name,
                     x.filename, m.linenumber,
                     msgmap[key]))
