mavlink/mavlink

commit 2c614f1c4134a60b0e748eb05f0b8eb50b498052

Make Numpy support optional.

pymavlink/mavutil.py
@@ -18,10 +18,6 @@ try:
 except Exception:
     pass
 
-# these imports allow for mavgraph and mavlogdump to use maths expressions more easily
-from math import *
-from .mavextra import *
-
 # maximum packet length for a single receive call - use the UDP limit
 UDP_MAX_PACKET_LEN = 65535
 

pymavlink/tools/mavgraph.py
@@ -9,7 +9,10 @@ import math, re
 import matplotlib
 from math import *
 
-from pymavlink.mavextra import *
+try:
+    from pymavlink.mavextra import *
+except:
+    print("WARNING: Numpy missing, mathematical notation will not be supported.")
 
 # cope with rename of raw_input in python3
 try:

pymavlink/tools/mavlogdump.py
@@ -9,6 +9,11 @@ header. The timestamp is in microseconds since 1970 (unix epoch)
 
 import sys, time, os, struct, json
 
+try:
+    from pymavlink.mavextra import *
+except:
+    print("WARNING: Numpy missing, mathematical notation will not be supported..")
+
 from argparse import ArgumentParser
 parser = ArgumentParser(description=__doc__)
 

commit 81db81a2afc89c848943f2241e9d47183c7a3424

pymavlink: fixed expression evaluation with mavextra and math
this was broken by 2c614f1c4134a60b0e748eb05f0b8eb50b498052

pymavlink/mavexpression.py
@@ -1,0 +1,34 @@
+#!/usr/bin/env python
+'''
+mavlink expression evaluation functions
+
+Copyright Andrew Tridgell 2011
+Released under GNU GPL version 3 or later
+'''
+
+import os
+
+# these imports allow for mavgraph and mavlogdump to use maths expressions more easily
+from math import *
+from .mavextra import *
+
+'''
+Support having a $HOME/.pymavlink/mavextra.py for extra graphing functions
+'''
+home = os.getenv('HOME')
+if home is not None:
+    extra = os.path.join(home, '.pymavlink', 'mavextra.py')
+    if os.path.exists(extra):
+        import imp
+        mavuser = imp.load_source('pymavlink.mavuser', extra)
+        from pymavlink.mavuser import *
+
+def evaluate_expression(expression, vars):
+    '''evaluation an expression'''
+    try:
+        v = eval(expression, globals(), vars)
+    except NameError:
+        return None
+    except ZeroDivisionError:
+        return None
+    return v

pymavlink/mavutil.py
@@ -7,7 +7,7 @@ Released under GNU GPL version 3 or later
 '''
 
 import socket, math, struct, time, os, fnmatch, array, sys, errno
-import select
+import select, mavexpression
 
 # adding these extra imports allows pymavlink to be used directly with pyinstaller
 # without having complex spec files. To allow for installs that don't have ardupilotmega
@@ -20,17 +20,6 @@ except Exception:
 
 # maximum packet length for a single receive call - use the UDP limit
 UDP_MAX_PACKET_LEN = 65535
-
-'''
-Support having a $HOME/.pymavlink/mavextra.py for extra graphing functions
-'''
-home = os.getenv('HOME')
-if home is not None:
-    extra = os.path.join(home, '.pymavlink', 'mavextra.py')
-    if os.path.exists(extra):
-        import imp
-        mavuser = imp.load_source('pymavlink.mavuser', extra)
-        from pymavlink.mavuser import *
 
 # Store the MAVLink library for the currently-selected dialect
 # (set by set_dialect())
@@ -53,13 +42,7 @@ def mavlink10():
 
 def evaluate_expression(expression, vars):
     '''evaluation an expression'''
-    try:
-        v = eval(expression, globals(), vars)
-    except NameError:
-        return None
-    except ZeroDivisionError:
-        return None
-    return v
+    return mavexpression.evaluate_expression(expression, vars)
 
 def evaluate_condition(condition, vars):
     '''evaluation a conditional (boolean) statement'''