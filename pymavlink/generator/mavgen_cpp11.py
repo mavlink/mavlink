15c8c001a28c7720e80afee3707ae3976521b49a
Vladimir Ermakov <vooon341@gmail.com>
2016-06-24 16:31:38 +0300

    c++11:test: Fix array init warnings

--- pymavlink/generator/mavgen_cpp11.py
+++ pymavlink/generator/mavgen_cpp11.py
@@ -345,8 +345,8 @@ def generate_one(basename, xml):
                 else:
                     f.to_yaml_code = """ss << "  %s: [" << to_string(%s) << "]" << std::endl;""" % (f.name, f.name)
 
-                    f.cxx_test_value = '{ %s }' % ', '.join([str(v) for v in f.test_value])
-                    f.c_test_value = f.cxx_test_value
+                    f.cxx_test_value = '{{ %s }}' % ', '.join([str(v) for v in f.test_value])
+                    f.c_test_value = '{ %s }' % ', '.join([str(v) for v in f.test_value])
             else:
                 f.cxx_type = f.type
                 f.to_yaml_code = """ss << "  %s: " << %s%s << std::endl;""" % (f.name, to_yaml_cast, f.name)
