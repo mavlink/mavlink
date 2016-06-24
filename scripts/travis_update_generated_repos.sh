89d76a4f5755cbb1d10d7a86cd3c24af1438dbbf
Lorenz Meier <lm@inf.ethz.ch>
2016-06-24 12:25:15 +0200

    Fix protocol generation and GIT upload

--- scripts/travis_update_generated_repos.sh
+++ scripts/travis_update_generated_repos.sh
@@ -36,13 +36,13 @@ mkdir -p include/mavlink/v2.0
 cd include/mavlink/v2.0
 git clone https://github.com/mavlink/c_library_v2.git
 cd ../../..
-./scripts/update_c_library.sh 2.0
+./scripts/update_c_library.sh 2
 # v1.0 legacy
 cd $GEN_START_PATH
 mkdir -p include/mavlink/v1.0
 cd include/mavlink/v1.0
 git clone https://github.com/mavlink/c_library_v1.git
 cd ../../..
-./scripts/update_c_library.sh 1.0
+./scripts/update_c_library.sh 1
 
 # XXX add build steps for other libraries as well
