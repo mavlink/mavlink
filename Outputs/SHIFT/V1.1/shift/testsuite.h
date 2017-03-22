d4f2a45ad70f592c1483ad65647b67242f01b172
Tom Vos <tom-v@live.nl>
2017-03-22 08:41:05 +0100

    commit so far

--- Outputs/SHIFT/V1.1/shift/testsuite.h
+++ Outputs/SHIFT/V1.1/shift/testsuite.h
@@ -9196,21 +9196,21 @@ static void mavlink_test_mav_shift_radiodog_ranging_request_ack_msg(uint8_t syst
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
-static void mavlink_test_mav_shift_radiodog_ranging_request_data_msg(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+static void mavlink_test_mav_shift_radiodog_ranging_request_data1_msg(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
     mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
-        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_MAV_SHIFT_RADIODOG_RANGING_REQUEST_DATA_MSG >= 256) {
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_MAV_SHIFT_RADIODOG_RANGING_REQUEST_DATA1_MSG >= 256) {
             return;
         }
 #endif
     mavlink_message_t msg;
         uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
         uint16_t i;
-    mavlink_mav_shift_radiodog_ranging_request_data_msg_t packet_in = {
+    mavlink_mav_shift_radiodog_ranging_request_data1_msg_t packet_in = {
         93372036854775807ULL,93372036854776311ULL,963498296,18275,199
     };
-    mavlink_mav_shift_radiodog_ranging_request_data_msg_t packet1, packet2;
+    mavlink_mav_shift_radiodog_ranging_request_data1_msg_t packet1, packet2;
         memset(&packet1, 0, sizeof(packet1));
         packet1.t1 = packet_in.t1;
         packet1.t2 = packet_in.t2;
@@ -9222,22 +9222,22 @@ static void mavlink_test_mav_shift_radiodog_ranging_request_data_msg(uint8_t sys
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
         if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
            // cope with extensions
-           memset(MAVLINK_MSG_ID_MAV_SHIFT_RADIODOG_RANGING_REQUEST_DATA_MSG_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_MAV_SHIFT_RADIODOG_RANGING_REQUEST_DATA_MSG_MIN_LEN);
+           memset(MAVLINK_MSG_ID_MAV_SHIFT_RADIODOG_RANGING_REQUEST_DATA1_MSG_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_MAV_SHIFT_RADIODOG_RANGING_REQUEST_DATA1_MSG_MIN_LEN);
         }
 #endif
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_mav_shift_radiodog_ranging_request_data_msg_encode(system_id, component_id, &msg, &packet1);
-    mavlink_msg_mav_shift_radiodog_ranging_request_data_msg_decode(&msg, &packet2);
+    mavlink_msg_mav_shift_radiodog_ranging_request_data1_msg_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_mav_shift_radiodog_ranging_request_data1_msg_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_mav_shift_radiodog_ranging_request_data_msg_pack(system_id, component_id, &msg , packet1.moduleId , packet1.radioId , packet1.moduleType , packet1.t1 , packet1.t2 );
-    mavlink_msg_mav_shift_radiodog_ranging_request_data_msg_decode(&msg, &packet2);
+    mavlink_msg_mav_shift_radiodog_ranging_request_data1_msg_pack(system_id, component_id, &msg , packet1.moduleId , packet1.radioId , packet1.moduleType , packet1.t1 , packet1.t2 );
+    mavlink_msg_mav_shift_radiodog_ranging_request_data1_msg_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_mav_shift_radiodog_ranging_request_data_msg_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.moduleId , packet1.radioId , packet1.moduleType , packet1.t1 , packet1.t2 );
-    mavlink_msg_mav_shift_radiodog_ranging_request_data_msg_decode(&msg, &packet2);
+    mavlink_msg_mav_shift_radiodog_ranging_request_data1_msg_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.moduleId , packet1.radioId , packet1.moduleType , packet1.t1 , packet1.t2 );
+    mavlink_msg_mav_shift_radiodog_ranging_request_data1_msg_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
@@ -9245,12 +9245,69 @@ static void mavlink_test_mav_shift_radiodog_ranging_request_data_msg(uint8_t sys
         for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
             comm_send_ch(MAVLINK_COMM_0, buffer[i]);
         }
-    mavlink_msg_mav_shift_radiodog_ranging_request_data_msg_decode(last_msg, &packet2);
+    mavlink_msg_mav_shift_radiodog_ranging_request_data1_msg_decode(last_msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
         
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_mav_shift_radiodog_ranging_request_data_msg_send(MAVLINK_COMM_1 , packet1.moduleId , packet1.radioId , packet1.moduleType , packet1.t1 , packet1.t2 );
-    mavlink_msg_mav_shift_radiodog_ranging_request_data_msg_decode(last_msg, &packet2);
+    mavlink_msg_mav_shift_radiodog_ranging_request_data1_msg_send(MAVLINK_COMM_1 , packet1.moduleId , packet1.radioId , packet1.moduleType , packet1.t1 , packet1.t2 );
+    mavlink_msg_mav_shift_radiodog_ranging_request_data1_msg_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
+static void mavlink_test_mav_shift_radiodog_ranging_request_data2_msg(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_MAV_SHIFT_RADIODOG_RANGING_REQUEST_DATA2_MSG >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_mav_shift_radiodog_ranging_request_data2_msg_t packet_in = {
+        93372036854775807ULL,963497880,17859,175
+    };
+    mavlink_mav_shift_radiodog_ranging_request_data2_msg_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.t4 = packet_in.t4;
+        packet1.moduleId = packet_in.moduleId;
+        packet1.radioId = packet_in.radioId;
+        packet1.moduleType = packet_in.moduleType;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_MAV_SHIFT_RADIODOG_RANGING_REQUEST_DATA2_MSG_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_MAV_SHIFT_RADIODOG_RANGING_REQUEST_DATA2_MSG_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_mav_shift_radiodog_ranging_request_data2_msg_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_mav_shift_radiodog_ranging_request_data2_msg_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_mav_shift_radiodog_ranging_request_data2_msg_pack(system_id, component_id, &msg , packet1.moduleId , packet1.radioId , packet1.moduleType , packet1.t4 );
+    mavlink_msg_mav_shift_radiodog_ranging_request_data2_msg_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_mav_shift_radiodog_ranging_request_data2_msg_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.moduleId , packet1.radioId , packet1.moduleType , packet1.t4 );
+    mavlink_msg_mav_shift_radiodog_ranging_request_data2_msg_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_mav_shift_radiodog_ranging_request_data2_msg_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_mav_shift_radiodog_ranging_request_data2_msg_send(MAVLINK_COMM_1 , packet1.moduleId , packet1.radioId , packet1.moduleType , packet1.t4 );
+    mavlink_msg_mav_shift_radiodog_ranging_request_data2_msg_decode(last_msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
@@ -9590,7 +9647,8 @@ static void mavlink_test_shift(uint8_t system_id, uint8_t component_id, mavlink_
     mavlink_test_mav_shift_radiodog_identification_msg(system_id, component_id, last_msg);
     mavlink_test_mav_shift_radiodog_ranging_request_msg(system_id, component_id, last_msg);
     mavlink_test_mav_shift_radiodog_ranging_request_ack_msg(system_id, component_id, last_msg);
-    mavlink_test_mav_shift_radiodog_ranging_request_data_msg(system_id, component_id, last_msg);
+    mavlink_test_mav_shift_radiodog_ranging_request_data1_msg(system_id, component_id, last_msg);
+    mavlink_test_mav_shift_radiodog_ranging_request_data2_msg(system_id, component_id, last_msg);
     mavlink_test_mav_shift_radiodog_module_cfg_msg(system_id, component_id, last_msg);
     mavlink_test_mav_shift_radiodog_px4_position_small(system_id, component_id, last_msg);
     mavlink_test_mav_shift_radiodog_debug_tag_info(system_id, component_id, last_msg);
