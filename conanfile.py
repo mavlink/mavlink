ff796526590d56412356d9fd3d327274d880db06
gal <gal@tevel-tech.com>
2020-01-22 09:55:38 +0000

    Wording fine tuning

--- conanfile.py
+++ conanfile.py
@@ -30,10 +30,10 @@ class MavlinkConan(ConanFile):
     description = "micro air vehicle communication protocol"
     topics = ("mavlink", "communication")
     exports_sources = "*"
-    options = {"msg_definitions": ["ardupilotmega", "ASLUAV", "autoquad", "common", "icarous", "matrixpilot",
+    options = {"msg_dialect": ["ardupilotmega", "ASLUAV", "autoquad", "common", "icarous", "matrixpilot",
                                     "minimal", "paparazzi", "python_array_test", "slugs", "standard", "test",
                                    "ualberta", "uAvionix"]}
-    default_options = ("msg_definitions = ardupilotmega")
+    default_options = ("msg_dialect = ardupilotmega")
     settings = "os", "compiler", "build_type", "arch"
 
     def source(self):
@@ -42,7 +42,7 @@ class MavlinkConan(ConanFile):
 
     def build(self):
         cmd = "python -m pymavlink.tools.mavgen --lang=C++11 --wire-protocol=2.0 --output=build message_definitions/v1.0/"\
-              + str(self.options.msg_definitions) + ".xml"
+              + str(self.options.msg_dialect) + ".xml"
         self.run(cmd)
 
     def package(self):
