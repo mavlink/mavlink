226dfa77484d5c8dfc3672a25f0448a8d762d2c6
Julian Oes <julian@oes.ch>
2021-02-03 16:33:06 +0100

    workflows/scripts: update deploy script

--- scripts/update_generated_repos.sh
+++ scripts/update_generated_repos.sh
@@ -1,27 +1,4 @@
-#!/bin/bash
-
-# Do only build for Python 2.7
-# as we only want to deploy for one
-# unique generator.
-PYTHONVER=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
-
-if [[ $PYTHONVER != "2.7"* ]]
-then
-	echo -e "Skipping header generation for Python $PYTHONVER"
-	exit 0
-fi
-
-# Do not build pull requests
-if [[ $TRAVIS_PULL_REQUEST != "false" ]]
-then
-	exit 0
-fi
-
-# Do only build master branch
-if [[ $TRAVIS_BRANCH != "master" ]]
-then
-	exit 0
-fi
+#!/usr/bin/env bash
 
 # Config for auto-building
 git remote rename origin upstream
@@ -44,5 +21,3 @@ cd include/mavlink/v1.0
 git clone https://github.com/mavlink/c_library_v1.git
 cd ../../..
 ./scripts/update_c_library.sh 1
-
-# XXX add build steps for other libraries as well
