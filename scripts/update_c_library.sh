e6b380da1661b1230b6cb6f2bbd9c6ba0e7d1442
Hamish Willee <hamishwillee@gmail.com>
2021-05-12 17:05:34 +1000

    update_c_library script - remove autoquad generation (#1630)

--- scripts/update_c_library.sh
+++ scripts/update_c_library.sh
@@ -61,7 +61,6 @@ rm -rf $CLIBRARY_PATH/*
 # generate new c headers
 echo -e "\0033[34mStarting to generate c headers\0033[0m\n"
 generate_headers ardupilotmega $1
-generate_headers autoquad $1
 generate_headers matrixpilot $1
 generate_headers test $1
 generate_headers ASLUAV $1
