551e3b491c05c0d3fa18124e2f5d7ac82616df51
Julian Oes <julian@oes.ch>
2022-09-07 14:53:19 +1200

    Fix CI (#1888)
    * workflows: remove Python 3.6 (EOL), add 3.9, 3.10
    
    * scripts: only format message_definitions
    
    Otherwise this tries to reformat xml files inside pymavlink which
    creates a circular dependency.
    
    * scripts: nicer formatting
    
    * workflows: the Python version is not a float

--- scripts/format_xml.sh
+++ scripts/format_xml.sh
@@ -6,7 +6,7 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
 # Initialize variables
 mode="format"
-xml_dir="."
+xml_dir="message_definitions"
 keep_old=0
 
 while getopts "h?cd:o" opt; do
@@ -34,7 +34,8 @@ then
 else
     xml_files="$xml_dir/$xml_file"
 fi
-echo "processing file(s) $xml_files"
+echo "processing file(s):"
+echo "$xml_files"
 
 ret=0
 for f in $xml_files
