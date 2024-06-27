2111689bc4968dffff1bf13632516b3ffc5148c0
RomanBapst <bapstroman@gmail.com>
2024-06-27 17:03:58 +0200

    fixed headers repo path
    Signed-off-by: RomanBapst <bapstroman@gmail.com>

--- scripts/update_generated_repos.sh
+++ scripts/update_generated_repos.sh
@@ -11,13 +11,13 @@ echo "https://${GH_TOKEN}:@github.com" > "$HOME"/.git-credentials
 GEN_START_PATH=$PWD
 mkdir -p include/mavlink/v2.0
 cd include/mavlink/v2.0
-git clone https://github.com/auterion/c_library_v2.git
+git clone https://github.com/Auterion/c_library_v2.git
 cd ../../..
 ./scripts/update_c_library.sh 2
 # v1.0 legacy
 cd "$GEN_START_PATH"
 mkdir -p include/mavlink/v1.0
 cd include/mavlink/v1.0
-git clone https://github.com/auterion/c_library_v1.git
+git clone https://github.com/Auterion/c_library_v1.git
 cd ../../..
 ./scripts/update_c_library.sh 1
