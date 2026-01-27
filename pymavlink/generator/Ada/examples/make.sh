d57c2b315548d93ed10e772f56987f7379bef67b
Wei <wei@avestec.com>
2026-01-26 16:12:57 -0800

    Convert pymavlink to submodule from ardupilot/pymavlink

--- pymavlink/generator/Ada/examples/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir build
-
-gnatmake -a --create-missing-dirs attitude.adb -D build -I..
-gnatmake -a --create-missing-dirs param_request_list.adb -D build -I..
-gnatmake -a --create-missing-dirs param_set_sr0_params -D build -I..
--- pymavlink/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.environ["MAVLINK20"] = "1"
